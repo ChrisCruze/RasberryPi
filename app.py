2c17f7c4909812530ed5dce6bf7394f9dada54a9
ChrisCruze <cruzc09@gmail.com>
2020-12-30 12:00:34 -0500

    fixed error with parsing

--- app.py
+++ app.py
@@ -40,8 +40,8 @@ CORS(server)
 class Speak(Resource):
     def get(self):
         args = request.args
-        parsed_json_data = request.json 
-        body_form = request.form 
+        # parsed_json_data = request.json 
+        # body_form = request.form 
         audioString = "testing 1 2 3"
         print(audioString)
         tts = gTTS(text=audioString, lang='en')
@@ -49,7 +49,15 @@ class Speak(Resource):
         sound = AudioSegment.from_mp3("audio.mp3")
         sound.export("audio.wav", format="wav")
         os.system("aplay audio.wav")
-        return body_form,parsed_json_data,request
+
+        #return body_form#,parsed_json_data,request
+        try:
+            return request.form
+        except Exception as err:
+            error_message = traceback.format_exc()
+            return error_message
+
+
 
 api.add_resource(Speak, '/speak')
 
